a8c66de97e6b1dc61ec2b176c5460b49a4402b69
gqq <gqq519@163.com>
2017-01-05 11:34:49 +0800

    百度地图视图的处理

--- app/src/main/java/com/feicuiedu/hunttreasure/treasure/map/MapFragment.java
+++ app/src/main/java/com/feicuiedu/hunttreasure/treasure/map/MapFragment.java
@@ -6,15 +6,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.BaiduMapOptions;
 import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
 import com.feicuiedu.hunttreasure.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by gqq on 2017/1/4.
@@ -25,18 +33,44 @@ public class MapFragment extends Fragment {
 
     @BindView(R.id.map_frame)
     FrameLayout mMapFrame;
+    @BindView(R.id.iv_located)
+    ImageView mIvLocated;
+    @BindView(R.id.btn_HideHere)
+    Button mBtnHideHere;
+    @BindView(R.id.centerLayout)
+    RelativeLayout mCenterLayout;
+    @BindView(R.id.iv_scaleUp)
+    ImageView mIvScaleUp;
+    @BindView(R.id.iv_scaleDown)
+    ImageView mIvScaleDown;
+    @BindView(R.id.tv_located)
+    TextView mTvLocated;
+    @BindView(R.id.tv_satellite)
+    TextView mTvSatellite;
+    @BindView(R.id.tv_compass)
+    TextView mTvCompass;
+    @BindView(R.id.tv_currentLocation)
+    TextView mTvCurrentLocation;
+    @BindView(R.id.iv_toTreasureInfo)
+    ImageView mIvToTreasureInfo;
+    @BindView(R.id.et_treasureTitle)
+    EditText mEtTreasureTitle;
+    @BindView(R.id.layout_bottom)
+    FrameLayout mLayoutBottom;
+    private BaiduMap mBaiduMap;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_map,container);
+        View view = inflater.inflate(R.layout.fragment_map, container);
+        return view;
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        ButterKnife.bind(this,view);
+        ButterKnife.bind(this, view);
 
         // 初始化百度地图
         initMapView();
@@ -63,12 +97,45 @@ public class MapFragment extends Fragment {
                 ;
 
         // 创建
-        MapView mapView = new MapView(getContext(),options);
+        MapView mapView = new MapView(getContext(), options);
 
         // 在布局上添加地图控件：0，代表第一位
-        mMapFrame.addView(mapView,0);
+        mMapFrame.addView(mapView, 0);
 
         // 拿到地图的操作类(控制器：操作地图等都是使用这个)
-        mapView.getMap();
+        mBaiduMap = mapView.getMap();
+    }
+
+    // 卫星视图和普通视图的切换
+    @OnClick(R.id.tv_satellite)
+    public void switchMapType(){
+        int mapType = mBaiduMap.getMapType();// 获取当前的地图类型
+        // 切换类型
+        mapType = (mapType==BaiduMap.MAP_TYPE_NORMAL)?BaiduMap.MAP_TYPE_SATELLITE:BaiduMap.MAP_TYPE_NORMAL;
+        // 卫星和普通的文字的显示
+        String msg  = mapType==BaiduMap.MAP_TYPE_NORMAL?"卫星":"普通";
+        mBaiduMap.setMapType(mapType);
+        mTvSatellite.setText(msg);
+    }
+
+    // 指南针
+    @OnClick(R.id.tv_compass)
+    public void switchCompass(){
+        // 指南针有没有显示
+        boolean compassEnabled = mBaiduMap.getUiSettings().isCompassEnabled();
+        mBaiduMap.getUiSettings().setCompassEnabled(!compassEnabled);
+    }
+
+    // 地图的缩放
+    @OnClick({R.id.iv_scaleDown,R.id.iv_scaleUp})
+    public void scaleMap(View view){
+        switch (view.getId()){
+            case R.id.iv_scaleDown:
+                mBaiduMap.setMapStatus(MapStatusUpdateFactory.zoomOut());
+                break;
+            case R.id.iv_scaleUp:
+                mBaiduMap.setMapStatus(MapStatusUpdateFactory.zoomIn());
+                break;
+        }
     }
 }
